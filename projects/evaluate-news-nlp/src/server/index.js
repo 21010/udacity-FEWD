59e29dc1967f680443b0a5fe2522350e3b050872
Grzegorz Ziolo <greg@ziolo.be>
2020-05-20 16:29:06 +0200

    adjust index.js

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,23 +1,35 @@
-var path = require('path')
+const dotenv = require('dotenv');
+dotenv.config();
+
+const path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const textApi = require('./utils/alyien.api')
 
 const app = express()
 
 app.use(express.static('dist'))
 
-console.log(__dirname)
-
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.get('/test', (req, res) => {
+    res.send(mockAPIResponse)
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+app.get('/hashtags', (req, res) => {
+    console.log(req)
+    // const { url } = req.body
+    // textApi.hashtags(url).then(result => res.send(result))
+})
+
+// aylienApi.summarize('https://en.wikipedia.org')
+//     .then(result => console.log(result))
+//     .catch(error => console.warn(error))
+
+// designates what port the app will listen to for incoming requests
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
 })
