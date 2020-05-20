80a077b56e867370d1b4550b43e507045732e088
Grzegorz Ziolo <greg@ziolo.be>
2020-05-20 20:19:20 +0200

    adjusting formatting

--- projects/evaluate-news-nlp/src/server/utils/alyien.api.js
+++ projects/evaluate-news-nlp/src/server/utils/alyien.api.js
@@ -14,13 +14,13 @@ function useTextApi (endpoint, params) {
 }
 
 module.exports = {
-    classify: (url) => useTextApi('classify', { url }),
+    classify:  (url) => useTextApi('classify', { url }),
     sentiment: (url) => useTextApi('sentiment', { url }),
-    taxonomy: (url) => useTextApi('classifyByTaxonomy', { url }),
-    concepts: (url) => useTextApi('concepts', { url }),
-    entities: (url) => useTextApi('entities', { url }),
-    extract: (url) => useTextApi('extract', { url }),
-    hashtags: (url) => useTextApi('hashtags', { url }),
-    language: (url) => useTextApi('language', { url }),
+    taxonomy:  (url) => useTextApi('classifyByTaxonomy', { url }),
+    concepts:  (url) => useTextApi('concepts', { url }),
+    entities:  (url) => useTextApi('entities', { url }),
+    extract:   (url) => useTextApi('extract', { url }),
+    hashtags:  (url) => useTextApi('hashtags', { url }),
+    language:  (url) => useTextApi('language', { url }),
     summarize: (url) => useTextApi('summarize', { url }),
 }
