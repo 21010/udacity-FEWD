be5879ce4f0ccdcc253a869d01deeee9ce059a8b
Grzegorz Ziolo <greg@ziolo.be>
2020-05-20 22:40:28 +0200

    fix the issue with regex expression

--- projects/evaluate-news-nlp/src/client/js/matchURL.js
+++ projects/evaluate-news-nlp/src/client/js/matchURL.js
@@ -1,5 +1,5 @@
 export function matchURL(url) {
-    const expression = '(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?'
+    const expression = /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?/
     const regex = new RegExp(expression)
     return url.match(regex) ? true : false
 }
