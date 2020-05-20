5f97694f5c346824c65bb500647eade1307cbe8d
Grzegorz Ziolo <greg@ziolo.be>
2020-05-20 22:42:20 +0200

    created the function to handle form submission

--- projects/evaluate-news-nlp/src/client/js/handleFormSubmit.js
+++ projects/evaluate-news-nlp/src/client/js/handleFormSubmit.js
@@ -1,5 +1,18 @@
-export function handleFormSubmit (event) {
+export function handleFormSubmit (event, type) {
     event.preventDefault()
-    alert('hi')
+
+    const url = document.getElementById('url').value
+
+    if (!client.matchURL(url)) return alert('url is incorrect')
+
+    console.log('loading...')
+
+    fetch(`//localhost:3000/api/${type}?url=${url}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log('data received')
+            client.renderResults({ type, url, data })
+        })
+        .catch(error => console.warn(error))
 }
 
