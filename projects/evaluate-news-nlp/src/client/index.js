0965d16d422a5db439f035bd2e198876f2c19f91
Grzegorz Ziolo <greg@ziolo.be>
2020-05-22 16:58:29 +0200

    add new function to the bundle

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -1,15 +1,17 @@
 import { matchURL } from './js/matchURL'
-import { handleFormSubmit } from './js/handleFormSubmit'
-import { renderResults } from './js/renderResults'
+import { handleFormSubmit, preventFormSubmit } from './js/handleFormSubmit'
+import { renderResults, scrollTop } from './js/renderResults'
 
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/header.scss'
-import './styles/form.scss'
+import './styles/main.scss'
 import './styles/footer.scss'
 
 export {
     matchURL,
     handleFormSubmit,
+    preventFormSubmit,
     renderResults,
+    scrollTop,
 }
