28645b5d87c948543f5a0ddd3d522a44a1ea6ee2
Grzegorz Ziolo <greg@ziolo.be>
2020-05-22 16:58:09 +0200

    adds DOM rendering functions

--- projects/evaluate-news-nlp/src/client/js/renderResults.js
+++ projects/evaluate-news-nlp/src/client/js/renderResults.js
@@ -1,29 +1,37 @@
-function renderResultsTitle (title) {
+export function renderResultsTitle (title) {
     const h3 = document.createElement('h3')
     h3.className = 'results-title'
     h3.textContent = title
     return h3
 }
 
+export function scrollTop() {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
+
 function renderHashtags ({ url, hashtags }) {
-    const parent = document.getElementById('hashtags')
+    const parent = document.querySelector('.results')
+    parent.innerHTML = ''
 
     const title = renderResultsTitle(`Hashtags generated for ${url}`)
     parent.appendChild(title)
 
     const hashtagsList = document.createElement('ul')
 
-    hashtags.forEach(hashtag => {
-        const listItem = document.createElement('li')
-        listItem.textContent = hashtag
-        hashtagsList.appendChild(listItem)
-    })
+    hashtags.sort((a, b) => b.length - a.length)
+        .forEach(hashtag => {
+            const listItem = document.createElement('li')
+            listItem.textContent = hashtag
+            hashtagsList.appendChild(listItem)
+        })
 
     parent.appendChild(hashtagsList)
 }
 
 function renderSummary ({ url, sentences }) {
-    const parent = document.getElementById('summary')
+    const parent = document.querySelector('.results')
+    parent.innerHTML = ''
 
     const title = renderResultsTitle(`Summary generated for ${url}`)
     parent.appendChild(title)
