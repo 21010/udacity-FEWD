e1539a524f61c7d999110236b6f5d8ab97aeace2
Grzegorz Ziolo <greg@ziolo.be>
2020-05-22 16:57:57 +0200

    add form submission JS functions

--- projects/evaluate-news-nlp/src/client/js/handleFormSubmit.js
+++ projects/evaluate-news-nlp/src/client/js/handleFormSubmit.js
@@ -1,18 +1,35 @@
+function showResults (state) {
+    const btn = document.getElementById('results-btn')
+    state ? btn.classList.remove('hidden') : btn.classList.add('hidden')
+    
+    const section = document.getElementById('results-section')
+    section.style.display = state ? 'flex' : 'none'
+}
+
+export function getData (type, url) {
+    return fetch(`//localhost:3000/api/${type}?url=${url}`)
+    .then(response => response.json())
+    // .then(data => data)
+}
+
+export function preventFormSubmit (event) {
+    event.preventDefault()
+    return false
+}
+
 export function handleFormSubmit (event, type) {
     event.preventDefault()
-
+    showResults(false)
     const url = document.getElementById('url').value
 
     if (!client.matchURL(url)) return alert('url is incorrect')
 
     console.log('loading...')
 
-    fetch(`//localhost:3000/api/${type}?url=${url}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log('data received')
-            client.renderResults({ type, url, data })
-        })
-        .catch(error => console.warn(error))
+    getData(type, url).then(data => {
+        console.log('data received')
+        client.renderResults({ type, url, data })
+        showResults(true)
+    }).catch(error => console.warn(error))
 }
 
