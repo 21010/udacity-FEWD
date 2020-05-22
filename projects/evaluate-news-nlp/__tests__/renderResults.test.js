dd62860320bac6253e09bd356ba3a1558215ae8c
Grzegorz Ziolo <greg@ziolo.be>
2020-05-22 21:54:13 +0200

    remove old files

--- projects/evaluate-news-nlp/__tests__/renderResults.test.js
+++
@@ -1,5 +0,0 @@
-import { renderResultsTitle } from '../src/client/js/renderResults'
-
-test('check if results title element is rendered', () => {
-    expect(typeof renderResultsTitle()).toBe('object')
-})
