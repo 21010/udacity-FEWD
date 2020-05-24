ce657541866a396541931e8a62f455820ec62c1b
= <=>
2020-05-24 18:23:31 +0200

    include database initiation

--- projects/travel-app-capstone/src/server/index.js
+++ projects/travel-app-capstone/src/server/index.js
@@ -4,7 +4,13 @@ dotenv.config()
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const api = require('./routes/api')
+const routes = {
+    api: require('./routes/api'),
+    travels: require('./routes/travels'),
+}
+
+const Travels = require('./data/travels')
+const travelsDB = new Travels()
 
 const port = 3000
 const app = express()
@@ -20,6 +26,7 @@ app.get('/', (req, res) => {
     res.send('dist/index.html')
 })
 
-app.use('/api', api)
+app.use('/api', routes.api)
+app.use('/travels', routes.travels(travelsDB))
 
 app.listen(port, () => console.log(`app is listening on http://localhost:${port}`))
