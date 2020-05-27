1b964b9a980022275934aab757e619cadf3f457a
Grzegorz Ziolo <greg@ziolo.be>
2020-05-27 15:00:53 +0200

    create geonames API endpoint

---
+++ projects/travel-app-capstone/src/server/utils/geonames.api.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch')
+const username = process.env.GEONAMES_USER
+
+function get (cityName) {
+    return fetch(`http://api.geonames.org/searchJSON?maxRows=10&q=${cityName}&username=${username}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.totalResultsCount = 0) return false
+            return data.geonames.map(row => {
+                return {
+                    name: row.name,
+                    lng: row.lng,
+                    lat: row.lat,
+                    countryName: row.countryName,
+                    countryCode: row.countryCode,
+                }
+            })
+        })
+}
+
+module.exports = {
+    get,
+}
