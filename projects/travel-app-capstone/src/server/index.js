70c692c668b64165214883414b5bdc8c128154c0
Grzegorz Ziolo <greg@ziolo.be>
2020-05-28 23:26:18 +0200

    fix the routes order

--- projects/travel-app-capstone/src/server/index.js
+++ projects/travel-app-capstone/src/server/index.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
@@ -22,11 +23,11 @@ app.use(cors())
 app.use(express.static('dist'))
 
 // routes
-app.get('/', (req, res) => {
-    res.send('dist/index.html')
-})
+app.get('/',    (req, res) => res.sendFile(path.resolve(__dirname, '../../dist/index.html')))
 
-app.use('/api/travels', routes.travels(travelsDB))
 app.use('/api', routes.api)
+app.use('/api/travels', routes.travels(travelsDB))
+
+app.get('*',    (req, res) => res.sendFile(path.resolve(__dirname, '../../dist/404.html')))
 
 app.listen(port, () => console.log(`app is listening on http://localhost:${port}`))
