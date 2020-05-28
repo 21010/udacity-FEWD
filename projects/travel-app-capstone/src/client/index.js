ee7d24c71bf45850702e3d5ec11782fc6f3dfc89
Grzegorz Ziolo <greg@ziolo.be>
2020-05-28 23:20:40 +0200

    imported js scripts for export to the client's library

--- projects/travel-app-capstone/src/client/index.js
+++ projects/travel-app-capstone/src/client/index.js
@@ -1,4 +1,8 @@
-
+import { handleMenuButton, resetEvent } from './js/menuButton'
+import { handleFormInputs, handleFormSubmit } from './js/addTripForm'
+import { eventListeners } from './js/eventListeners'
+import AppState from './js/AppState'
+import { fetchTrips } from './js/fetchTrips'
 
 // import SASS files
 import './styles/base.scss'
@@ -8,11 +12,18 @@ import './styles/page-main.scss'
 import './styles/page-footer.scss'
 import './styles/trips-list.scss'
 import './styles/trips-list-item.scss'
+import './styles/add-trip.scss'
 
 // import media
 import logo from './media/logo.svg'
 
 // export library
-module.export = {
-    
+export {
+    handleMenuButton,
+    resetEvent,
+    handleFormInputs,
+    handleFormSubmit,
+    eventListeners,
+    fetchTrips,
+    AppState,
 }
