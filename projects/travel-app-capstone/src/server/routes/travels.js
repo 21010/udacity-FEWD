e4b0509ea2758911d2dca12ed96651990d790a4f
Grzegorz Ziolo <greg@ziolo.be>
2020-05-28 23:26:52 +0200

    fix the travels routes order

--- projects/travel-app-capstone/src/server/routes/travels.js
+++ projects/travel-app-capstone/src/server/routes/travels.js
@@ -1,20 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const { createBundle } = require('../utils/bundler')
 
 module.exports = (travels) => {
+
+    router.get('/get', (req, res) => {
+        res.json(travels.getAll())
+    })
 
     router.get('/get/:id', (req, res) => {
         const { id } = req.params
         res.json(travels.get(id))
     })
 
-    router.get('/get', (req, res) => {
-        res.json(travels.getAll())
-    })
-
     router.post('/add', (req, res) => {
-        const { data } = req.body
-        if (data) res.json(travels.add(data))
+        const { location, date } = req.body
+        createBundle(location, date)
+            .then(bundle => {
+                res.json(travels.add(bundle))
+            })
+            .catch(error => console.warn(error))
     })
 
     router.post('/remove/:id', (req, res) => {
