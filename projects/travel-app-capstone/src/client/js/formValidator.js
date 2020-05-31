2d796a2584dbc45832f7d6aa8fdc5408963cd499
Grzegorz Ziolo <greg@ziolo.be>
2020-05-31 15:57:15 +0200

    fix the bug with checking the returned data

--- projects/travel-app-capstone/src/client/js/formValidator.js
+++ projects/travel-app-capstone/src/client/js/formValidator.js
@@ -2,7 +2,7 @@ export function validateData (location) {
     return fetch(`//localhost:3000/api/geonames/${location}`)
         .then(response => response.json())
         .then(data => {
-            if (data.lenght === 0) return false
+            if (!data) return false
             const lat = data[0].lat
             const lng = data[0].lng
             return fetch(`//localhost:3000/api/weather?lat=${lat}&lng=${lng}`)
@@ -11,5 +11,8 @@ export function validateData (location) {
                     return false
                 })
         })
-        .catch(error => console.warn(error))
+        .catch(error => {
+            console.warn(error)
+            return false
+        })
 }
